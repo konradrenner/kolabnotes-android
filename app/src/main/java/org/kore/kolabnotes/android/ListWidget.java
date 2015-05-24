d51126e3f55fe7a38dd9543e220dadd79d290e12
Konrad Renner <koni.renner@gmail.com>
2015-05-24 18:45:04 +0200

    continued working on list widget

--- app/src/main/java/org/kore/kolabnotes/android/ListWidget.java
+++ app/src/main/java/org/kore/kolabnotes/android/ListWidget.java
@@ -46,7 +46,7 @@ public class ListWidget extends AppWidgetProvider {
         CharSequence widgetText = ListWidgetConfigureActivity.loadTitlePref(context, appWidgetId);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.list_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+        //views.setTextViewText(R.id.appwidget_text, widgetText);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
--- app/src/main/java/org/kore/kolabnotes/android/ListWidgetConfigureActivity.java
+++ app/src/main/java/org/kore/kolabnotes/android/ListWidgetConfigureActivity.java
@@ -1,5 +1,7 @@
 package org.kore.kolabnotes.android;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.app.Activity;
 import android.appwidget.AppWidgetManager;
 import android.content.Context;
@@ -7,7 +9,21 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
+
+import org.kore.kolab.notes.Notebook;
+import org.kore.kolab.notes.Tag;
+import org.kore.kolabnotes.android.content.NotebookRepository;
+import org.kore.kolabnotes.android.content.TagRepository;
+import org.kore.kolabnotes.android.security.AuthenticatorActivity;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 
 /**
@@ -16,9 +32,21 @@ import android.widget.EditText;
 public class ListWidgetConfigureActivity extends Activity {
 
     int mAppWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
-    EditText mAppWidgetText;
-    private static final String PREFS_NAME = "org.kore.kolabnotes.android.ListWidget";
+    public static final String PREFS_NAME = "org.kore.kolabnotes.android.ListWidget";
     private static final String PREF_PREFIX_KEY = "appwidget_";
+
+    private TagRepository tagRepository = new TagRepository(this);
+    private NotebookRepository notebookRepository = new NotebookRepository(this);
+    private AccountManager mAccountManager;
+    private Spinner accountSpinner;
+    private Spinner notebookSpinner;
+    private Spinner tagSpinner;
+
+    private Account selectedAccount;
+    private String selectedNotebook;
+    private String selectedTag;
+
+    private String localAccountName;
 
     public ListWidgetConfigureActivity() {
         super();
@@ -33,8 +61,13 @@ public class ListWidgetConfigureActivity extends Activity {
         setResult(RESULT_CANCELED);
 
         setContentView(R.layout.list_widget_configure);
-        mAppWidgetText = (EditText) findViewById(R.id.appwidget_text);
         findViewById(R.id.add_button).setOnClickListener(mOnClickListener);
+
+        accountSpinner = (Spinner) findViewById(R.id.spinner_account);
+        notebookSpinner = (Spinner) findViewById(R.id.spinner_notebook);
+        tagSpinner = (Spinner) findViewById(R.id.spinner_tag);
+
+        localAccountName = getResources().getString(R.string.drawer_account_local);
 
         // Find the widget id from the intent.
         Intent intent = getIntent();
@@ -50,7 +83,9 @@ public class ListWidgetConfigureActivity extends Activity {
             return;
         }
 
-        mAppWidgetText.setText(loadTitlePref(ListWidgetConfigureActivity.this, mAppWidgetId));
+        mAccountManager = AccountManager.get(this);
+        initSpinners();
+        //mAppWidgetText.setText(loadTitlePref(ListWidgetConfigureActivity.this, mAppWidgetId));
     }
 
     View.OnClickListener mOnClickListener = new View.OnClickListener() {
@@ -58,8 +93,8 @@ public class ListWidgetConfigureActivity extends Activity {
             final Context context = ListWidgetConfigureActivity.this;
 
             // When the button is clicked, store the string locally
-            String widgetText = mAppWidgetText.getText().toString();
-            saveTitlePref(context, mAppWidgetId, widgetText);
+            //String widgetText = mAppWidgetText.getText().toString();
+            //saveTitlePref(context, mAppWidgetId, widgetText);
 
             // It is the responsibility of the configuration activity to update the app widget
             AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
@@ -74,7 +109,7 @@ public class ListWidgetConfigureActivity extends Activity {
     };
 
     // Write the prefix to the SharedPreferences object for this widget
-    static void saveTitlePref(Context context, int appWidgetId, String text) {
+    static void saveTitlePref(Context context, int appWidgetId,String text) {
         SharedPreferences.Editor prefs = context.getSharedPreferences(PREFS_NAME, 0).edit();
         prefs.putString(PREF_PREFIX_KEY + appWidgetId, text);
         prefs.commit();
@@ -96,6 +131,134 @@ public class ListWidgetConfigureActivity extends Activity {
         SharedPreferences.Editor prefs = context.getSharedPreferences(PREFS_NAME, 0).edit();
         prefs.remove(PREF_PREFIX_KEY + appWidgetId);
         prefs.commit();
+    }
+
+    void initSpinners(){
+        initAccountSpinner();
+        updateNotebookSpinner();
+        updateTagSpinner();
+    }
+
+    void initAccountSpinner(){
+        Account[] accounts = mAccountManager.getAccountsByType(AuthenticatorActivity.ARG_ACCOUNT_TYPE);
+
+        String[] accountNames = new String[accounts.length+1];
+
+        accountNames[0] = localAccountName;
+        for(int i=0; i< accounts.length;i++){
+            accountNames[i+1] = mAccountManager.getUserData(accounts[i],AuthenticatorActivity.KEY_ACCOUNT_NAME);
+        }
+
+        Arrays.sort(accountNames,1,accountNames.length);
+
+        ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(this,R.layout.widget_config_spinner_item,accountNames);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        accountSpinner.setAdapter(adapter);
+        accountSpinner.setOnItemSelectedListener(new OnAccountItemClicked());
+        accountSpinner.setSelection(0);
+    }
+
+    class OnAccountItemClicked implements AdapterView.OnItemSelectedListener{
+
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            String name = parent.getSelectedItem().toString();
+
+            if(localAccountName.equalsIgnoreCase(name)){
+                selectedAccount = null;
+            }else{
+                Account[] accounts = mAccountManager.getAccountsByType(AuthenticatorActivity.ARG_ACCOUNT_TYPE);
+
+                for(Account account : accounts){
+                    if(name.equals(mAccountManager.getUserData(account, AuthenticatorActivity.KEY_ACCOUNT_NAME))){
+                        selectedAccount = account;
+                        break;
+                    }
+                }
+            }
+
+            updateNotebookSpinner();
+            updateTagSpinner();
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+            //nothing
+        }
+    }
+
+    void updateNotebookSpinner(){
+        String rootFolder;
+        String email;
+        if(selectedAccount == null){
+            rootFolder = "Notes";
+            email = "local";
+        }else{
+            rootFolder = mAccountManager.getUserData(selectedAccount,AuthenticatorActivity.KEY_ROOT_FOLDER);
+            email = mAccountManager.getUserData(selectedAccount,AuthenticatorActivity.KEY_EMAIL);
+        }
+
+        List<Notebook> notebooks = new ArrayList<>(notebookRepository.getAll(email, rootFolder));
+
+        Collections.sort(notebooks);
+
+        String[] notebookArr = new String[notebooks.size()+1];
+        notebookArr[0] = getResources().getString(R.string.no_selection);
+        for(int i=0; i<notebooks.size();i++){
+            notebookArr[i+1] = notebooks.get(i).getSummary();
+        }
+
+        ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(this, R.layout.widget_config_spinner_item, notebookArr);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        notebookSpinner.setAdapter(adapter);
+        notebookSpinner.setSelection(0);
+        notebookSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectedNotebook = parent.getSelectedItem().toString();
+
+                if(getResources().getString(R.string.no_selection).equals(selectedNotebook)){
+                    selectedNotebook = null;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                //nothing
+            }
+        });
+    }
+
+    void updateTagSpinner(){
+        List<String> tags = tagRepository.getAll();
+
+        Collections.sort(tags);
+
+        String[] tagArr = new String[tags.size()+1];
+        tagArr[0] = getResources().getString(R.string.no_selection);
+        for(int i=0;i<tags.size();i++){
+            tagArr[i+1] = tags.get(i);
+        }
+
+        ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(this, R.layout.widget_config_spinner_item, tagArr);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        tagSpinner.setAdapter(adapter);
+        tagSpinner.setSelection(0);
+        tagSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectedTag = parent.getSelectedItem().toString();
+
+                if(getResources().getString(R.string.no_selection).equals(selectedTag)){
+                    selectedTag = null;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                //nothing
+            }
+        });
     }
 }
 
